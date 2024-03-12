e40a781113dc977d57a53e73d2993f66740937aa
Sam <shortdriva@aol.com>
2024-03-11 22:14:38 -0400

    set up retrofit instance

--- app/src/main/java/com/cs4520/assignment4/Api.kt
+++ app/src/main/java/com/cs4520/assignment4/Api.kt
@@ -1,6 +1,30 @@
 package com.cs4520.assignment4
 
+import com.cs4520.assignment4.logic.Product
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 object Api {
     const val BASE_URL: String = "https://kgtttq6tg9.execute-api.us-east-2.amazonaws.com/"
     const val ENDPOINT: String = "prod/"
+}
+
+object RetrofitBuilder {
+    private lateinit var retrofit: Retrofit
+
+    fun getRetrofit(): Retrofit {
+        if (!this::retrofit.isInitialized) {
+            retrofit = Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return retrofit
+    }
+}
+
+interface ApiService {
+    @GET(Api.ENDPOINT)
+    suspend fun getAllProducts(): List<Product>
 }
