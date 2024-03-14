6b7d7db89fc5811302264063a2989e9f49161223
Sam <shortdriva@aol.com>
2024-03-14 01:00:36 -0400

    updated DAO interface with methods needed for caching product data

--- app/src/main/java/com/cs4520/assignment4/model/ProductDao.kt
+++ app/src/main/java/com/cs4520/assignment4/model/ProductDao.kt
@@ -3,11 +3,21 @@ package com.cs4520.assignment4.model
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 
 @Dao
 interface ProductDao {
     @Insert
-    fun addProduct(product: Product)
+    fun addProducts(products: List<Product>)
+
+    @Query("DELETE FROM Product")
+    fun clearProducts()
+
+    @Transaction
+    fun replaceCache(products: List<Product>) {
+        clearProducts()
+        addProducts(products)
+    }
 
     @Query("SELECT * FROM Product")
     fun getAllProducts(): List<Product>
