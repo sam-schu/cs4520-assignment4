b382b0e7b1093e93f9f75ce96f6ee187f777f8f3
Sam <shortdriva@aol.com>
2024-03-14 18:09:16 -0400

    fixed database not being created

--- app/src/main/java/com/cs4520/assignment4/ui/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment4/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cs4520.assignment4.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cs4520.assignment4.R
+import com.cs4520.assignment4.model.ApiAdventuresDatabaseProvider
 
 /**
  * The application's single activity, which can host both the login and product list fragments.
@@ -10,6 +11,7 @@ import com.cs4520.assignment4.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ApiAdventuresDatabaseProvider.setContext(applicationContext)
         setContentView(R.layout.main_activity)
     }
 }
